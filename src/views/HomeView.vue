4c499639eaa24084cd31d2faa4e201f0feb5ac8b
Your Name <you@example.com>
2023-04-16 14:30:00 +0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <appModal v-model="dialog">
+          <h1>salom</h1>
+        </appModal>
+        <button @click="dialog = true">open modal</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+import { onMounted, ref } from 'vue';
+import appModal from '@/components/ui/app-modal.vue';
+import { mapGetters } from 'vuex';
+import { computed ,watch} from 'vue';
+const store = useStore()
+const count = computed(() => store.getters.getCounter)
+const auth = computed(() => store.getters.getAuth)
+const dialog = ref(false)
+const myAction = () => {
+  store.dispatch('addCounter')
+}
+onMounted(()=>{
+  // store.dispatch('getTodos')
+})
+
+</script>
